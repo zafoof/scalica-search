d3bb19ec69d5aa63e52c19f6744709262e40b9f6
zafoof <bsule321@gmail.com>
2017-12-14 16:24:10 -0500

    added remote ip

--- web/scalica/rpc_search/index/indexer.py
+++ web/scalica/rpc_search/index/indexer.py
@@ -5,7 +5,7 @@ from inspect import getmembers
 _TIMEOUT_SECONDS = 10
 
 def index_post(post_id, text):
-  	channel = grpc.insecure_channel('localhost:22222')
+  	channel = grpc.insecure_channel('54.172.21.215:22222')
 	stub = index_pb2_grpc.IndexerStub(channel)
 	request = index_pb2.IndexPostRequest(post_id=post_id, text=text)
 	response = stub.index(request, _TIMEOUT_SECONDS)
