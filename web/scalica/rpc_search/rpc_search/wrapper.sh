b7640a1c6391f73d5d5886bbbc5027e80cecfb8c
zafoof <bsule321@gmail.com>
2017-12-14 16:16:18 -0500

    wrong command

--- web/scalica/rpc_search/rpc_search/wrapper.sh
+++ web/scalica/rpc_search/rpc_search/wrapper.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-until ${web/scalica/rpc_search/rpc_search/wrapper.sh}; do
+until ${python web/scalica/rpc_search/rpc_search/search_service.py}; do
 	echo "Search service crashed. Restarting"
 done
