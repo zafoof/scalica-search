3f906893273aec1aa110809be88f3b24b18efb16
root <root@ip-172-31-82-206.ec2.internal>
2017-12-14 21:20:08 +0000

    syntax

--- web/scalica/rpc_search/index/wrapper.sh
+++ web/scalica/rpc_search/index/wrapper.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-until ${python web/scalica/rpc_search/index/index_service.py}; do
+until python web/scalica/rpc_search/index/index_service.py; do
 	echo "Indexer crashed. Restarting"
 done
--- web/scalica/rpc_search/rpc_search/wrapper.sh
+++ web/scalica/rpc_search/rpc_search/wrapper.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-until ${python web/scalica/rpc_search/rpc_search/search_service.py}; do
+until python web/scalica/rpc_search/rpc_search/search_service.py; do
 	echo "Search service crashed. Restarting"
 done
